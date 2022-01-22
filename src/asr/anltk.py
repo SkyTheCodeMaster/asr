05c7bd65c3ffebe5533b19ab1052ef2e6afa9f19
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2022-01-22 13:42:11 -0500

    Add async methods for heavy NLTK methods

---
+++ src/asr/anltk.py
@@ -0,0 +1,20 @@
+# Provides async versions of heavy nltk functions
+
+import asyncio
+import concurrent.futures
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+lemmatizer = WordNetLemmatizer()
+
+async def run(callable,*,executor:concurrent.futures.ProcessPoolExecutor=concurrent.futures.ProcessPoolExecutor):
+  loop = asyncio.get_running_loop()
+  with executor() as pool:
+    return loop.run_in_executor(pool,callable)
+
+async def lemmatize(word:str,*,**kwargs):
+  return await run(lambda:lemmatizer(word),**kwargs)
+
+async def download(package:str,*,**kwargs):
+  return await run(lambda:nltk.download(package),**kwargs)
