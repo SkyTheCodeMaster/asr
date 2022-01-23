313e0b65cb19e98f0c753ec8163040300b5eb654
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2022-01-22 23:06:44 -0500

    Add NLP module

---
+++ src/asr/nlp.py
@@ -0,0 +1,28 @@
+import nltk
+from nltk.tokenize import word_tokenize
+
+import anltk
+
+
+class NLP:
+  def extractNe(self,text):
+    words = word_tokenize(text)
+    tags = nltk.pos_tag(words)
+    tree = nltk.ne_chunk(tags,binary=True)
+    return set(
+      " ".join(i[0] for i in t)
+      for t in tree
+      if hasattr(t,"label") and t.label() == "NE"
+    )
+
+  def extractNeTyped(self,text):
+    words = word_tokenize(text)
+    tags = nltk.pos_tag(words)
+    bTree = nltk.ne_chunk(tags,binary=True)
+    tree = nltk.ne_chunk(tags)
+    ntags = []
+    for i,v in enumerate(bTree):
+      if hasattr(v,"label") and v.label() == "NE":
+        t = tree[i]
+        ntags.append({"type":t.label(),"name":t[0]})
+    return ntags
